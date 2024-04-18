ed51150c020c56ecc06f775054b4d9c402779854
toki1616 <tokigit@gmail.com>
2024-04-19 00:09:04 +0900

    novelのメッセージ表示の下にセーブなどのボタンを生成する処理の追加

---
+++ UI/Assets/Scripts/Novel/View/UnderUI/NovelUnderUIView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class NovelUnderUIView : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _spawnPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateUnderButton();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreateUnderButton()
+    {
+        Array array = Enum.GetValues(typeof(NovelUnderButtonEnum.Menu));
+        foreach (var menuValue in array.Cast<NovelUnderButtonEnum.Menu>())
+        {
+            GameObject spawnObject = Instantiate(_spawnPrefab, this.transform);
+            spawnObject.GetComponent<NovelUnderButtonView>().SetMenuEnum(menuValue);
+        }
+    }
+}
