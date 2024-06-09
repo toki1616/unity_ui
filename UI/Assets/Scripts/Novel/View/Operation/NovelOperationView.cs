44aacdd72811a52cc9fc4d93f8c6695ae74e4733
toki1616 <tokigit@gmail.com>
2024-06-09 13:48:58 +0900

    UIを隠す処理の追加

---
+++ UI/Assets/Scripts/Novel/View/Operation/NovelOperationView.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using Zenject;
+
+public class NovelOperationView : MonoBehaviour
+{
+    private NovelPresenter _novelPresenter;
+
+    [Inject]
+    public void Construct
+        (
+        NovelPresenter novelPresenter
+        )
+    {
+        _novelPresenter = novelPresenter;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _novelPresenter.isUIHidden.Subscribe(_ => ChangeOperationUIActive(_)).AddTo(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ChangeOperationUIActive(bool isActive)
+    {
+        this.gameObject.SetActive(isActive);
+    }
+}
